eb41fe8dc4e59f263b01b45e2d1bc588a3e9edaa
Nirosha55 <niroshaniru323@gmail.com>
2024-02-06 14:57:11 +0530

    added

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="container">
+            <h1>Welcome to our app!</h1>
+            <p>This is a simple example of how you can use react-router with TypeScript</p>
+        </div>
+    )
+}
+
+export default App;
