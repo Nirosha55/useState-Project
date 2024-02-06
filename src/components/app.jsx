07d0b20d8f83f70a1bcb6def1c52cf8cdd361fae
Nirosha55 <niroshaniru323@gmail.com>
2024-02-06 15:00:12 +0530

    aa

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,6 @@ function App() {
     return (
         <div className="container">
             <h1>Welcome to our app!</h1>
-            <p>This is a simple example of how you can use react-router with TypeScript</p>
         </div>
     )
 }
