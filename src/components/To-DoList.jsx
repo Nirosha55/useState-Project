4d8b756033a3580a725b20d0342d43a4fcc7fd37
Nirosha55 <niroshaniru323@gmail.com>
2024-02-14 18:50:56 +0530

    adding new files

--- src/components/To-DoList.jsx
+++ src/components/To-DoList.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
+import todoItem from '../components/todoItem';
 
 function ToDoApp() {
+
+const  [inputText, setInputText] = React.useState("");
+const  [items, setItems] = React.useState([]);
+
+
+
+function handleChange(event) {
+const newValue = event.target.value;
+setInputText(newValue);
+
+}
+
+
+function addItem(){
+setItems((prevValue) => {
+    return[...prevValue,inputText ];
+});
+}
+
     return(
         <div className='container'>
           <div className='heading'>
           <h1>To-Do List</h1>
           </div>
           <div className='form'>
-            <input type='text'/>
-            <button>Add Task</button>
+            <input onChange={handleChange} type='text' value={inputText}/>
+            <button onClick={addItem}>Add Task</button>
           </div>
           <ul>
-            <li>A item</li>
+          {items.map(todoItem =>(<todoItem text= {todoItem}/>))}
           </ul>
         </div>
     );
 }
-
-
-export default ToDoApp;
+       export default ToDoApp;
---
+++ src/components/todoItem.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function todoItem() {
+return<li>Text</li>
+}
+
+
+export default todoItem;
